c9f406592eeed16422b1220af0f04f18d44f027b
servoo <servettakmaz@gmail.com>
2021-07-16 20:50:58 +0200

    L 19 through 21. Implementing a controller

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,30 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+	private final BookRepository bookRepository;
+
+	public BookController(final BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+
+	/**
+	 * why Model? What does this do?
+	 */
+	@RequestMapping("/books")
+	public String getBooks(Model model) {
+
+		/**
+		 * What does this exactly do?
+		 */
+		model.addAttribute("books", bookRepository.findAll());
+
+		return "books";
+	}
+}
